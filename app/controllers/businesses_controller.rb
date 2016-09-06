0d347f3532ac787f5ade11b23c96f0ca773ded41
matt carlson and matthew giem <matt+matthew@>
2016-09-06 10:58:27 -0700

    create the edit and destroy paths for businesses

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -14,6 +14,27 @@ class BusinessesController < ApplicationController
     end
   end
 
+  def edit
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    if @business.update(business_params)
+      redirect_to type_path(@business.type)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  redirect_to type_path(@business.type)
+  end
+
   private
   def business_params
     params.require(:business).permit(:name, :phone, :address, :website, :hours)
